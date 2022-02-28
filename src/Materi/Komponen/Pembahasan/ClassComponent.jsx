f17c5b56b28715a32075ce59a093fd4ee92a8e26
fauzangalib <ozangaib@gmail.com>
2022-02-28 15:50:33 +0700

    using state at class component

--- src/Materi/Komponen/Pembahasan/ClassComponent.jsx
+++ src/Materi/Komponen/Pembahasan/ClassComponent.jsx
@@ -1,21 +1,14 @@
 import React from "react";
 
 class ClassComponent extends React.Component {
-    constructor(props){
-        super(props)
-        
-        this.state = {
-            value : 0
-        }
-
-        this.handleplus = this.handleplus.bind(this);
-        this.handleminus = this.handleminus.bind(this);
+    state = {
+        value : 0
     }
 
-    handleplus(){
+    handleplus = () =>{
         this.setState({value: this.state.value + 1})
     }
-    handleminus(){
+    handleminus =() =>{
         if(this.state.value > 0){
             this.setState({value: this.state.value - 1})
         }
